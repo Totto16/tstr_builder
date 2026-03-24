720b56c19a0af648958edf43eaa7c19adbae56a2
Totto16 <tobiausgais@gmail.com>
2026-03-24 21:23:28 +0100

    feat: use newer tmap compare interface, instead of cmp use eq function

--- src/utils/tmap_common_impl.c
+++ src/utils/tmap_common_impl.c
@@ -7,14 +7,14 @@ TMAP_HASH_FUNC_SIG(tstr, TSTR_KEYNAME) {
 	return TMAP_HASH_BYTES(tstr_cstr(&key), tstr_len(&key));
 }
 
-TMAP_COMPARE_FUNC_SIG(tstr, TSTR_KEYNAME) {
-	return tstr_cmp(&key1, &key2);
+TMAP_EQ_FUNC_SIG(tstr, TSTR_KEYNAME) {
+	return tstr_eq(&key1, &key2);
 }
 
 TMAP_HASH_FUNC_SIG(tstr_view, TStringView) {
 	return TMAP_HASH_BYTES(key.data, key.len);
 }
 
-TMAP_COMPARE_FUNC_SIG(tstr_view, TStringView) {
-	return tstr_view_cmp(key1, key2);
+TMAP_EQ_FUNC_SIG(tstr_view, TStringView) {
+	return tstr_view_eq_view(key1, key2);
 }
